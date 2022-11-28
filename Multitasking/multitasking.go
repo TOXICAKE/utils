b5ea2b9eaf64560e2ad4a14e65b23d301769d460
B9O2 <112383606+B9O2@users.noreply.github.com>
2022-11-28 17:05:41 +0800

    tag

--- Multitasking/multitasking.go
+++ Multitasking/multitasking.go
@@ -20,37 +20,37 @@ type Multitasking struct {
 	ctx        context.Context
 }
 
-func (lrm *Multitasking) AddTask(taskInfo interface{}) {
-	lrm.taskQueue <- taskInfo
+func (m *Multitasking) AddTask(taskInfo interface{}) {
+	m.taskQueue <- taskInfo
 	utils.DebugEcho("Join task successfully")
 }
 
-func (lrm *Multitasking) Register(taskFunc func(), execFunc func(interface{}, context.Context) interface{}) {
-	lrm.taskFunc = taskFunc
-	lrm.execFunc = execFunc
+func (m *Multitasking) Register(taskFunc func(), execFunc func(interface{}, context.Context) interface{}) {
+	m.taskFunc = taskFunc
+	m.execFunc = execFunc
 }
 
-func (lrm *Multitasking) Run() ([]interface{}, error) {
+func (m *Multitasking) Run() ([]interface{}, error) {
 	var result []interface{}
-	utils.DebugEcho("Try run module '" + lrm.name + "'...")
-	if lrm.taskFunc == nil || lrm.execFunc == nil {
-		errors.New("Multitasking '" + lrm.name + "' must be registered")
+	utils.DebugEcho("Try run module '" + m.name + "'...")
+	if m.taskFunc == nil || m.execFunc == nil {
+		errors.New("Multitasking '" + m.name + "' must be registered")
 	}
 
 	go func() {
-		lrm.taskFunc()
-		close(lrm.taskQueue)
+		m.taskFunc()
+		close(m.taskQueue)
 		utils.DebugEcho("Task-thread closed.")
 	}() //启动任务写入进程
 	utils.DebugEcho("Start task-thread")
 
-	for i := 0; i < lrm.threads; { //启动任务执行进程
-		lrm.wg.Add(1)
+	for i := 0; i < m.threads; { //启动任务执行进程
+		m.wg.Add(1)
 		go func(tid int) {
-			defer lrm.wg.Done()
-			for task := range lrm.taskQueue {
+			defer m.wg.Done()
+			for task := range m.taskQueue {
 				utils.DebugEcho(tid, "::Execute task <"+reflect.TypeOf(task).String()+">"+fmt.Sprintf("%s", task))
-				lrm.resultChan <- lrm.execFunc(task, lrm.ctx)
+				m.resultChan <- m.execFunc(task, m.ctx)
 				utils.DebugEcho(tid, "::Task <"+reflect.TypeOf(task).String()+">"+fmt.Sprintf("%s", task)+" Done")
 			}
 			utils.DebugEcho(tid, "::Exec-thread closed.")
@@ -60,7 +60,7 @@ func (lrm *Multitasking) Run() ([]interface{}, error) {
 	}
 	stop := make(chan struct{})
 	go func() { //启动执行结果读取进程
-		for r := range lrm.resultChan {
+		for r := range m.resultChan {
 			result = append(result, r)
 		}
 		utils.DebugEcho("Result-thread closed.")
@@ -68,8 +68,8 @@ func (lrm *Multitasking) Run() ([]interface{}, error) {
 	}()
 	utils.DebugEcho("Started result-thread")
 
-	lrm.wg.Wait()
-	close(lrm.resultChan)
+	m.wg.Wait()
+	close(m.resultChan)
 	<-stop
 	return result, nil
 }
